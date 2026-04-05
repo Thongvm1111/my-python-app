e3020c99bacc352dc7d0ced81535eaa40b6d4f03
thongvm <thongvm@lovedevops.pro>
2026-04-05 17:20:11 +0700

    Update message to v2.0

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return {
-        "message": "Hello from my-python-app!",
+        "message": "Hello from my-python-app! Version 2.0",
         "version": os.getenv("APP_VERSION", "1.0.0")
     }
 
